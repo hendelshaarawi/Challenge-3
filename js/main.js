aa2d4695e23421f1209f0301008d283f9ffa7c4f
hendelshaarawi <hendelshaarawi@mbp-van-hend.wireless.hhs.nl>
2018-04-05 18:56:51 +0200

    New

--- js/main.js
+++ js/main.js
@@ -1,124 +1,23 @@
-var script = document.createElement('script');
-
-script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
-// EARTHQUAKE VISUALISATION
-
 var map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
-        center: {
-            lat: 20.02472,
-            lng: -75.82194
-        },
-
-        mapTypeId: 'terrain',
-
-
-        styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
-
-    });
-
   var geoJSON;
   var request;
   var gettingData = false;
-  var openWeatherMapKey = "276d063af925739d0b65eb3f733bdf35"
+  var openWeatherMapKey = "276d063af925739d0b65eb3f733bdf35";
 
   function initialize() {
     var mapOptions = {
-      zoom: 2,
-      center: new google.maps.LatLng(50,-50)
+      zoom: 6,
+      center: new google.maps.LatLng(20.02472,-75.82194)
     };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'),
+
+
+    map = new google.maps.Map(document.getElementById('map'),
         mapOptions);
-    // Add interaction listeners to make weather requests
+    // Als je klikt, een listener die dn info laadt
     google.maps.event.addListener(map, 'idle', checkIfDataRequested);
 
-    // Sets up and populates the info window with details
+    // Info met icoon, stad, temperatuur en het weer plaatsen in window
     map.data.addListener('click', function(event) {
       infowindow.setContent(
        "<img src=" + event.feature.getProperty("icon") + ">"
@@ -141,7 +40,7 @@ function initMap() {
   }
 
   var checkIfDataRequested = function() {
-    // Stop extra requests being sent
+    // Niet extra aanvragen doen
     while (gettingData === true) {
       request.abort();
       gettingData = false;
@@ -149,7 +48,7 @@ function initMap() {
     getCoords();
   };
 
-  // Get the coordinates from the Map bounds
+  // Coordinaten ophalen en plaatsen
   var getCoords = function() {
     var bounds = map.getBounds();
     var NE = bounds.getNorthEast();
@@ -157,7 +56,7 @@ function initMap() {
     getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
   };
 
-  // Make the weather request
+  // Ophalen van het weer
   var getWeather = function(northLat, eastLng, southLat, westLng) {
     gettingData = true;
     var requestString = "https://api.openweathermap.org/data/2.5/box/city?bbox="
@@ -172,7 +71,7 @@ function initMap() {
     request.send();
   };
 
-  // Take the JSON results and proccess them
+  // Json ophalen en tonen
   var proccessResults = function() {
     console.log(this);
     var results = JSON.parse(this.responseText);
@@ -187,7 +86,7 @@ function initMap() {
 
   var infowindow = new google.maps.InfoWindow();
 
-  // For each result that comes back, convert the data to geoJSON
+  // Iconen voor het weer
   var jsonToGeoJson = function (weatherItem) {
     var feature = {
       type: "Feature",
@@ -211,7 +110,36 @@ function initMap() {
         coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
       }
     };
-    // Set the custom marker icon
+
+
+       var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h3 id="firstHeading" class="firstHeading">Havana, Cuba</h3>' +
+        '<div id="bodyContent">' +
+        '<p><b>José Martí International Airport</b> is een internationale luchthaven, gelegen op 15 km buiten de stad Havana in <b>Cuba</b>. ' +
+        'Het is een hub voor verschillende Cubaanse luchtvaartmaatschappijen. </p>' +
+
+        '<p>Attribution:  <a target="_blank" href="https://nl.wikipedia.org/wiki/Cuba_(land)">Cuba</a> ' + ' &#x1F1E8;&#x1F1FA; ' +
+
+        '(last visited June 22, 2009).</p>' +
+        '</div>' +
+        '</div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+ marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {
+            lat: 22.989166,
+            lng: -82.409164
+        }
+    });
+
+    // Custom marker zetten
     map.data.setStyle(function(feature) {
       return {
         icon: {
@@ -221,7 +149,7 @@ function initMap() {
       };
     });
 
-    // returns object
+    // return object
     return feature;
   };
 
@@ -244,132 +172,3 @@ function initMap() {
   };
 
   google.maps.event.addDomListener(window, 'load', initialize);
-
-    //José Martí International Airport, is een internationale luchthaven, gelegen op 15 km buiten de stad Havana in Cuba. Het is een hub voor verschillende Cubaanse luchtvaartmaatschappijen
-
-// '<div id="siteNotice">'+
-//             '</div>'+
-//             '<h3 id="firstHeading" class="firstHeading">Havana, Cuba</h3>'+
-//             '<div id="bodyContent">'+
-//             '<p><b>José Martí International Airport</b>, is een internationale luchthaven, gelegen op 15 km buiten de stad Havana in <b>Cuba</b>,  Het is een hub voor verschillende Cubaanse luchtvaartmaatschappijen' +
-//             +
-//             '<p>Attribution: Cuba, <a href="https://nl.wikipedia.org/wiki/Cuba_(land)"></a></p> '+
-//             '</div>'+
- //           '</div>';
-
-//22.989166, -82.409164
-
-
-
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h3 id="firstHeading" class="firstHeading">Havana, Cuba</h3>'+
-            '<div id="bodyContent">'+
-            '<p><b>José Martí International Airport</b> is een internationale luchthaven, gelegen op 15 km buiten de stad Havana in <b>Cuba</b>. ' +
-            'Het is een hub voor verschillende Cubaanse luchtvaartmaatschappijen. </p>'+
-            
-            '<p>Attribution:  <a target="_blank" href="https://nl.wikipedia.org/wiki/Cuba_(land)">Cuba</a> '+ ' &#x1F1E8;&#x1F1FA; '+
-           
-            '(last visited June 22, 2009).</p>'+
-            '</div>'+
-            '</div>';
-       var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-     
-    marker = new google.maps.Marker({
-          map: map,
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          position: {lat: 22.989166,
-            lng: -82.409164}
-        });
-       // marker.addListener('click', toggleBounce);
-
-         marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-
-         function toggleBounce() {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-      }
-
-
-    // Maken van een script-HTML tag 
-    var script = document.createElement('script');
-
-    //Link van de aardbevingen-locator
-  //Invoeren waar de source vandaan komt en stoppen van scripttag in head
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
-
-    //De CSS van de inforamatie weergeven
-    //Krijgt een cirkel, op basis van de kans -> grote of kleine cirkel (magnitude)
-    map.data.setStyle(function(feature) {
-        var magnitude = feature.getProperty('mag');
-        return {
-            icon: getCircle(magnitude)
-        };
-    });
-}
-
-//Hoe de resultaten gezien worden (cirkel met de style)
-function getCircle(magnitude) {
-    return {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: '#ff5719',
-        fillOpacity: .5,
-        scale: Math.pow(2, magnitude) / 1.8,
-        strokeColor: 'black',
-        strokeWeight: .5
-    };
-
-    
-}
-function eqfeed_callback(results) {
-    map.data.addGeoJson(results);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//               WEATHER                   // KEY=276d063af925739d0b65eb3f733bdf35
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
